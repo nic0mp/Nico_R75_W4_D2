3f67f287763d94f3b6e4ec3f9c5451c067d946cf
Nico Pinguel <nico.mp215@gmail.com>
2021-11-02 17:42:53 -0500

    table created

--- D2HW.sql
+++ D2HW.sql
@@ -3,17 +3,39 @@ CREATE TABLE customer(
 	first_name VARCHAR(100),
 	last_name VARCHAR(100),
 	billing_info VARCHAR(150),
-	email_info VARCHAR(150),
+	email_info VARCHAR(150)
 );
 
-CREATE cart(
-	cart_id SERIAL PRIMARY KEY,
-	item_id INTEGER
-	ticket_id INTEGER
-	FOREIGN KEY(item_id) REFERENCES concession(item_id)
-	FOREIGN KEY(ticket_id) REFERENCES ticket(ticket_id)
+CREATE TABLE concession(
+	item_id SERIAL PRIMARY KEY,
+	item_amount INTEGER,
+	upc INTEGER
 );
 
-CREATE concession(
+CREATE TABLE movie(
+	movie_id SERIAL PRIMARY KEY,
+	title VARCHAR,
+	rating VARCHAR,
+	description VARCHAR,
+	release_date DATE
+
+);
+
+CREATE TABLE ticket(
+	ticket_id SERIAL PRIMARY KEY,
+	ticket_amount INTEGER,
+	show_time TIME,
+	movie_id INTEGER,
+	FOREIGN KEY(movie_id) REFERENCES movie(movie_id)
 	
+);
+
+CREATE TABLE cart(
+	cart_id SERIAL PRIMARY KEY,
+	item_id INTEGER,
+	ticket_id INTEGER,
+	customer_id INTEGER,
+	FOREIGN KEY(item_id) REFERENCES concession(item_id),
+	FOREIGN KEY(ticket_id) REFERENCES ticket(ticket_id),
+	FOREIGN KEY(customer_id) REFERENCES customer(customer_id)
 );
